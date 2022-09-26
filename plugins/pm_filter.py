b72c67d767afcd070de032ee7fadced695d559ad
NAFIH KC (CAPTAIN) <103903674+nafihkc@users.noreply.github.com>
2022-09-26 07:57:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -780,7 +780,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+    await msg.reply("<b>ഇതിൽ ഏതെങ്കിലും ആണോ? 👇</b>",
                     reply_markup=InlineKeyboardMarkup(btn))
 
 
